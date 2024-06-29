6c852a57e44cac53403d25cad5669fb3d2020847
Gael Mahe <gaelmahe1@hotmail.com>
2024-06-29 23:55:52 +0200

    Logo added | Auth works but i will need to optimize the process

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Topbar = () => {
@@ -7,6 +6,7 @@ const Topbar = () => {
     <section className="topbar">
       <div className="flex-between py-4 px-5">
       <Link to="/" className="flex gap-3 items-center">
+      <img src="/assets/images/logo.svg" alt="logo" />
       </Link>
       </div>
     </section>
